78ececc0c558366757cca21d2544a37b1e31905e
Oluniyisrael <israeloluniyi2004@gmail.com>
2024-11-22 10:56:58 +0100

    Hehe

--- App.js
+++ App.js
@@ -1,28 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+import Landing from "./pages/landing/Landing";
+
 
 export default function App() {
   const handlePress = () =>{
     console.log("Button pressed!!")
   }
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <TouchableOpacity title= "Hi" style={styles.button} onPress={handlePress}>Click me!!</TouchableOpacity>
-      <StatusBar style="auto" />
-    </View>
+    <Landing/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button: {
-    backgroundColor:"red",
-    padding: "20px"
-  }
-});
---
+++ pages/landing/Landing.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { TouchableOpacity,  Text, View } from 'react-native';
+import styles from './LandingStyle';
+
+
+export default function Landing() {
+    return(
+        <View style={styles.container}>
+        <Text>Hello World!</Text>
+        <TouchableOpacity title= "Hi" style={styles.button} onPress={handlePress}>Click me!!</TouchableOpacity>
+        <StatusBar style="auto" />
+      </View>
+    )    
+}
