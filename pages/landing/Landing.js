578856dfd473b7c312aead35e998413aa28c8e07
Oluniyisrael <israeloluniyi2004@gmail.com>
2024-11-22 16:04:07 +0100

    Nye

--- pages/landing/Landing.js
+++ pages/landing/Landing.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { TouchableOpacity,  Text, View } from 'react-native';
+import { TouchableOpacity, Text, View } from 'react-native';
 import styles from './LandingStyle';
 import { handlePress } from '../../functions/functions';
 
 export default function Landing() {
-    return(
-        <View style={styles.container}>
-        <Text>Hello World!</Text>
-        <TouchableOpacity title= "Hi" style={styles.button} onPress={handlePress}>Click me!!</TouchableOpacity>
-        <StatusBar style="auto" />
-      </View>
-    )    
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+      <TouchableOpacity title="Hi" style={styles.button} onPress={handlePress}>
+        <Text>
+          Click me!!
+        </Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  )
 }
